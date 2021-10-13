2ddc96675b84e0950825f056cdd9150c27a327ba
Kiru <mail@kiru.gay>
2021-10-13 11:19:38 +0000

    new: prefix and suffix
    Instead of having "https://google.com" being written out as the visible
    URL you can set the prefix to "https://" and the URL to just
    "google.com" it will then appear as "google.com" but navigate with the
    protocol, allowing for a little bit better UI.
    
    Besides this if you need to navigate to a specific page on the domain
    you can add it in the suffix.

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 async function load() {
 	let prefs = await (await fetch("settings.json")).json();
-	try {
-		prefs = await (await fetch("custom-settings.json")).json();
-	}catch(err) {}
 
 	document.title = prefs.title;
 
 	for (let i = 0; i < prefs.pages.length; i++) {
 		main.innerHTML += `<div class="header">${prefs.pages[i].title}</div>`
 		for (let ii = 0; ii < prefs.pages[i].links.length; ii++) {
-			main.innerHTML += `<div class="links"><div class="link link${i + '' + ii}">${prefs.pages[i].links[ii].name}</div></div>`;
+			let pref = prefs.pages[i].links[ii];
+
+			main.innerHTML += `<div class="links"><div class="link link${i + '' + ii}">${pref.name}</div></div>`;
 			let link = document.querySelector(".link" + i + '' + ii);
 
+			if (! pref.prefix) {pref.prefix = ""}
+			if (! pref.suffix) {pref.suffix = ""}
 			if (! prefs.hideurls) {
 				if (! prefs.pages[i].hideurls) {
-					if (! prefs.pages[i].links[ii].hideurl) {
-						if (prefs.pages[i].links[ii].port != "") {
-							link.innerHTML += `<div class="url">on port ${prefs.pages[i].links[ii].port}</div>`
-							link.setAttribute("onclick", `location.href = "${location.protocol}//${window.location.hostname}:${prefs.pages[i].links[ii].port}"`);
-						} else if (prefs.pages[i].links[ii].url != "") {
-							link.innerHTML += `<div class="url">${prefs.pages[i].links[ii].url}</div>`
-							link.setAttribute("onclick", `location.href = "${prefs.pages[i].links[ii].url}"`);
+					if (! pref.hideurl) {
+						if (pref.port != "") {
+							link.innerHTML += `<div class="url">on port ${pref.port}</div>`
+							link.setAttribute("onclick", `location.href = "${pref.prefix}${location.protocol}//${window.location.hostname}:${pref.port}${pref.suffix}"`);
+						} else if (pref.url != "") {
+							link.innerHTML += `<div class="url">${pref.url}</div>`
+							link.setAttribute("onclick", `location.href = "${pref.prefix}${pref.url}${pref.suffix}"`);
 						}
 					}
 				}
